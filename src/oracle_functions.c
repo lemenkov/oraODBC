e2a1a890aa505229db7dae3205e0e56621365180
Dennis Box <dbox@fnal.gov>
2004-08-06 21:04:03 +0000

    credit where credit is due

--- src/oracle_functions.c
+++ src/oracle_functions.c
@@ -18,10 +18,14 @@
 		   Dennis Box (Fermi Nat Accelerator Lab)
                    Stefan.Radman@ctbto.org
 
+		   Steven Reynolds , sreynolds@bradmark.com
+
+		   Gary Houston, ghouston@intellecthr.com
+
 		   *
  *******************************************************************************
  *
- * $Id: oracle_functions.c,v 1.29 2004/08/06 20:46:10 dbox Exp $
+ * $Id: oracle_functions.c,v 1.30 2004/08/06 21:04:03 dbox Exp $
  * NOTE
  * There is no mutexing in these functions, it is assumed that the mutexing 
  * will be done at a higher level
@@ -31,7 +35,7 @@
 #include "ocitrace.h"
 #include <sqlext.h>
 
-static char const rcsid[]= "$RCSfile: oracle_functions.c,v $ $Revision: 1.29 $";
+static char const rcsid[]= "$RCSfile: oracle_functions.c,v $ $Revision: 1.30 $";
 
 /*
  * There is a problem with a lot of libclntsh.so releases... an undefined
