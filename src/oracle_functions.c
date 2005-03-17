650e9a144af117bbab935dada54a352dd4d0644c
Dennis Box <dbox@fnal.gov>
2005-03-17 01:36:32 +0000

    Changes submitted by Phillipe Le Rohellec <phillipe@cloudmark.com>        In the function that converts LOBs to string, ocilob_sqllvc,        the length of the field is first retrieved by a call to        OCILobGetLength which is good, but right after that the amtp        variable is set to that size minus the offset and the offset is 1.
           amtp tells the OCILobRead function how much data it needs to
           read and amtp is off by 1 which explain my truncated reads.
    
           The fix is to change
           amtp=ir->lobsiz-ir->posn;
           to
           amtp=ir->lobsiz-ir->posn+1;

--- src/oracle_functions.c
+++ src/oracle_functions.c
@@ -25,7 +25,7 @@
 		   *
  *******************************************************************************
  *
- * $Id: oracle_functions.c,v 1.34 2005/02/21 19:43:53 dbox Exp $
+ * $Id: oracle_functions.c,v 1.35 2005/03/17 01:36:32 dbox Exp $
  * NOTE
  * There is no mutexing in these functions, it is assumed that the mutexing 
  * will be done at a higher level
@@ -35,7 +35,7 @@
 #include "ocitrace.h"
 #include <sqlext.h>
 
-static char const rcsid[]= "$RCSfile: oracle_functions.c,v $ $Revision: 1.34 $";
+static char const rcsid[]= "$RCSfile: oracle_functions.c,v $ $Revision: 1.35 $";
 
 /*
  * There is a problem with a lot of libclntsh.so releases... an undefined
@@ -2510,7 +2510,7 @@ SQLRETURN ocilob_sqllvc(int row,ir_T* ir ,SQLPOINTER target,
   OCILobGetLength(ir->desc->stmt->dbc->oci_svc,ir->desc->stmt->dbc->oci_err,
 		  ir->locator[row],&ir->lobsiz);
 
-  amtp=ir->lobsiz-ir->posn;
+  amtp=ir->lobsiz-ir->posn+1;
   if(amtp>(unsigned)buflen)
     amtp=(unsigned)buflen;
 
